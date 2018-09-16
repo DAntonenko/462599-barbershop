e26be928f4b5fe4e33b2be4c37ab7fa79e093063
Danton <dm.antonenko1977@gmail.com>
2018-09-17 01:00:44 +0300

    Модальные окна доделаны, ошибки исправлены: финал
    Появление, анимация и мелкие улучшения модальных окон. В каталоге кнопки убраны из ссылок: теперь ссылкой в карточке товара является только фото. Frameborder у iframe заменён на цсс-свойство. HTML верифицирован, CSS префиксирован и минимизирован, JS скомпрессирован. Проект полностью готов.

--- js/modals.js
+++ js/modals.js
@@ -1,5 +1,6 @@
 var loginLink = document.querySelector(".login-link");
-var mapLink = document.querySelector(".map-link");
+var mapLink_1 = document.querySelector(".map-link_1");
+var mapLink_2 = document.querySelector(".map-link_2");
 
 var loginBox = document.querySelector(".modal-login");
 var mapBox = document.querySelector(".modal-map");
@@ -7,21 +8,76 @@ var mapBox = document.querySelector(".modal-map");
 var loginClose = document.querySelector(".login-close");
 var mapClose = document.querySelector(".map-close");
 
+var loginInput = loginBox.querySelector("[name=login]");
+var passwordInput = loginBox.querySelector("[name=password]");
+var form = loginBox.querySelector("form");
+
+var isStorageSupport = true;
+var storageLogin = localStorage.getItem("login");
+try {
+	storageLogin = localStorage.getItem("login");
+} catch (err) {
+isStorageSupport = false;
+}
+
 loginLink.addEventListener("click", function(evt) {
 	evt.preventDefault();
-	loginBox.classList.remove("modal-hide"); 
+	loginBox.classList.remove("modal-hide");
+	loginBox.classList.add("modal-show-bounce");
+	loginInput.focus();
+
+	if (storageLogin) {
+		loginInput.value = storageLogin;
+		passwordInput.focus();
+	}
 });
 
-mapLink.addEventListener("click", function() {
-	mapBox.classList.remove("modal-hide");
+form.addEventListener("submit", function (evt) {
+	if (!loginInput.value) {
+		evt.preventDefault();
+		loginBox.classList.add("modal-error");
+	} else {
+		localStorage.setItem("login", loginInput.value);
+	}
+
+	if (!passwordInput.value) {
+		evt.preventDefault();
+		loginBox.classList.add("modal-error");
+	}
 });
 
 loginClose.addEventListener("click", function(evt) {
 	evt.preventDefault();
 	loginBox.classList.add("modal-hide");
+	loginBox.classList.remove("modal-error");
+});
+
+mapLink_1.addEventListener("click", function() {
+	mapBox.classList.remove("modal-hide");
+	mapBox.classList.add("modal-show-scale");
+});
+
+mapLink_2.addEventListener("click", function() {
+	mapBox.classList.remove("modal-hide");
+	mapBox.classList.add("modal-show-scale");
 });
 
 mapClose.addEventListener("click", function(evt) {
 	evt.preventDefault();
 	mapBox.classList.add("modal-hide");
+});
+
+window.addEventListener("keydown", function (evt) {
+	if (evt.keyCode === 27) {
+		evt.preventDefault();
+
+		if (!loginBox.classList.contains("modal-hide")) {
+			loginBox.classList.add("modal-hide");
+			loginBox.classList.remove("modal-error");
+		}
+
+		if (!mapBox.classList.contains("modal-hide")) {
+			mapBox.classList.add("modal-hide");
+		}
+	}
 });
